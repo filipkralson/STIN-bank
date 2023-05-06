7b0e94d6a5be3306af524bb609d0a5ab799727bc
filip.kralson <filip.kral1@tul.cz>
2023-05-06 21:03:09 +0200

    #3 add controllers for login and register

--- Bank/src/main/java/cz/tul/kral/bank/controller/BankController.java
+++ Bank/src/main/java/cz/tul/kral/bank/controller/BankController.java
@@ -1,23 +1,52 @@
 package cz.tul.kral.bank.controller;
 
 import cz.tul.kral.bank.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@RestController
+@Controller
 public class BankController {
 
-    private List<User> users = List.of(new User("Tonda", "Svacina", "tonda.svacina@gmail.com"));
+    private List<User> users = new ArrayList<>();
 
-    @GetMapping("/users")
-    public List<User> get() {
-        return users;
+    @GetMapping("/home")
+    public String showHomePage() {
+        return "home";
     }
 
-    @GetMapping("/")
-    public String hello() {
-        return "Hello bank user!";
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegister(@RequestParam("name") String name, @RequestParam("surname") String surname, @RequestParam("email") String email, @RequestParam("password") String password) {
+        if ((name != null) && (surname != null )&&(email != null)&&(password != null)) {
+            users.add(new User(321, name, surname, email, password));
+            return "redirect:/login";
+        } else {
+            return "redirect:/register?error=true";
+        }
+    }
+
+
+    @PostMapping("/login")
+    public String processLogin(@RequestParam("clientNum") String clientNum, @RequestParam("password") String password) {
+        List<User> users = new ArrayList<>();
+        users.add(new User(123,"tonda","svacina","email@email.cz","123"));
+        for (User user : users) {
+            if (clientNum.equals(user.getClientNum()) && password.equals(user.getPassword())) {
+                return "redirect:/home"; // Redirect to home page if login successful
+            }
+        }
+        return "redirect:/login?error=true"; // Redirect back to login page with error parameter if login unsuccessful
     }
 }
+
--- Bank/src/main/java/cz/tul/kral/bank/model/User.java
+++ Bank/src/main/java/cz/tul/kral/bank/model/User.java
@@ -1,14 +1,18 @@
 package cz.tul.kral.bank.model;
 
 public class User {
+    private int clientNum;
     private String firstName;
     private String lastName;
     private String email;
+    private String password;
 
-    public User(String firstName, String lastName, String email) {
+    public User(int clientNum, String firstName, String lastName, String email, String password) {
+        this.clientNum = clientNum;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.password = password;
     }
 
     public String getFirstName() {
@@ -21,6 +25,14 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+
+    public int getClientNum() {
+        return clientNum;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
 
