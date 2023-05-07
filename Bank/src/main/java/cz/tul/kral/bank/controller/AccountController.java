1fdd582174ac5a84c10efdc3ef2fde74974262a1
filip.kralson <filip.kral1@tul.cz>
2023-05-07 10:41:04 +0200

    initial

--- Bank/src/main/java/cz/tul/kral/bank/controller/AccountController.java
+++ Bank/src/main/java/cz/tul/kral/bank/controller/AccountController.java
@@ -1,4 +1,13 @@
 package cz.tul.kral.bank.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AccountController {
+
+    @GetMapping("/create-account")
+    public String showCreateAccount() {
+        return "create-account";
+    }
 }
